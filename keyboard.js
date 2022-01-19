dc8628399f31f4c99e70359ade02f99b0c5062ae
Ester <ester.martinez@xarxatecactiva.com>
2022-01-19 22:30:53 +0100

    estilos y algunas media

--- keyboard.js
+++ keyboard.js
@@ -5,10 +5,7 @@ function keybFisic (callback) {
     document.addEventListener("keydown", (e)=>{
         if((e.which>=65 && e.which<=90) || e.which==13 || e.which==8 || e.which==165){
             callback(e.key); 
-        }
-       
-        
-         
+        }   
     });
 }
 
