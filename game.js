b4d17431f770e6abedf8592bd39c5c210ee92193
Ester <ester.martinez@xarxatecactiva.com>
2022-01-20 19:54:55 +0100

    alerts dark-info y limpieza

--- game.js
+++ game.js
@@ -3,8 +3,23 @@ import { randomWord, checkWord } from "./wordles2.js";
 
 let word = randomWord();
 console.log(word);
-let possible = 1;
+let round = 1;
 let temporalWord = "";
+const spanToggle = document.querySelector('#toggle-theme');
+const spanInfo = document.querySelector('#info');
+const divHidden= document.getElementById('hidden')
+
+spanToggle.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+    
+});
+spanInfo.addEventListener('click', ()=>{
+    if(divHidden.classList.contains("hidden")){
+        divHidden.classList.replace('hidden', 'show');
+    }else {
+        divHidden.classList.replace('show', 'hidden');
+    }
+})
 
 
 
@@ -12,17 +27,9 @@ function writeposition(id, value) {
     document.getElementById(id).innerText = value.toUpperCase();
 }
 function paint(position, res) {
-/*     document.getElementById(position + "1").style.backgroundColor = res.value[0];
-    document.getElementById(position + "2").style.backgroundColor = res.value[1];
-    document.getElementById(position + "3").style.backgroundColor = res.value[2];
-    document.getElementById(position + "4").style.backgroundColor = res.value[3];
-    document.getElementById(position + "5").style.backgroundColor = res.value[4]; */
     for (let i = 0; i < 5; i++) {
-
         document.getElementById(position + "" + (i + 1)).style.backgroundColor = res.value[i];
         document.getElementById(position + "" + (i + 1)).style.color = "white";
-        
-
     }
 }
 function paintKeybord (arrResponse){
@@ -32,8 +39,7 @@ function paintKeybord (arrResponse){
       letter= temporalWord.charAt(i);
       button = document.getElementById(letter); 
       if (!button.classList.contains("bg-green")) {
-          //button.classList.add()
-        button.className="tecla"; 
+            button.className="tecla"; 
         if (arrResponse[i]=="#787C7E") {
             button.classList.add("bg-grey"); 
         }
@@ -50,22 +56,20 @@ function paintKeybord (arrResponse){
 function restartVirtualKeyboard (){
     let keys= document.getElementsByClassName("tecla");
     for (let i = 0; i < keys.length; i++) {
-        keys[i].className="tecla";
-
-        
+        keys[i].className="tecla"; 
     }
 
 }
-function finishGame (possible) {
-    if (possible>6) {
-        alert("Has perdido prigao. Tu palabra era" + "" + word);
+function finishGame (round) {
+    if (round>6) {
+        swal("Se acabaron los intentos. Tu palabra era " + "-" + word );
         resetGame();
 
     }
 }
 function resetGame() {
     word= randomWord();
-    possible=1;
+    round=1;
     temporalWord=""; 
 
     for (let i = 1; i <= 6; i++) {
@@ -77,14 +81,14 @@ function resetGame() {
         
     }
     restartVirtualKeyboard();
-
+    console.log(word);
 
 }
 function makeWord(key) {
     //console.log(key);
     if (key == "Backspace") {
         if (temporalWord != "") {
-            writeposition(possible + "" + temporalWord.length, " ");
+            writeposition(round + "" + temporalWord.length, " ");
             temporalWord = temporalWord.slice(0, -1)
         }
     } else if (key == "Enter") {
@@ -95,33 +99,31 @@ function makeWord(key) {
             let wined = true;
             for (let i = 0; i < response.value.length && wined; i++) {
                 wined = response.value[i] == "#6AAA64" && wined;
-                paint(possible, response);  
+                paint(round, response);  
             }
             if (!wined) {
-                possible++;
-                finishGame(possible);
+                round++;
+                finishGame(round);
                 temporalWord= "";
                 
             } else {
                setTimeout(()=>{
-                alert("Enhorabuena");
+                swal("¡Enhorabuena crack!");
                 resetGame();
                 },500);
-
-                
             }
 
         }else {
-            alert(response.value);
+            swal(response.value);
         }
         
     } else {
         if (temporalWord.length < 5) {
             temporalWord += key;
-            writeposition(possible + "" + temporalWord.length, key);
+            writeposition(round + "" + temporalWord.length, key);
         }
 
-    } console.log(temporalWord);
+    } 
 }
 
 
